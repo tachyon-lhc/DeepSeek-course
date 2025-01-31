d86f5c42f401a407b31be6c3488701cbfc062807
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2025-01-31 13:27:21 -0300

    Ejercicios resueltos

--- modulo-1/1-numpy/ejercicio-1-1.py
+++ modulo-1/1-numpy/ejercicio-1-1.py
@@ -21,15 +21,35 @@ Ejercicio 2: Indexación y Slicing:
     - Extrae la submatriz de las dos primeras filas y las dos primeras columnas.
 '''
 matrix_1 = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-
-print(matrix_1)
-# segunda fila
-print(f"Segunda fila: {matrix_1[1, :]}")
-# tercera columna
-print(f"Tercera columna: {matrix_1[:, 2]}")
-# dos primeras filas, dos ultimas columnas
-print(f"Submatriz: \n{matrix_1[:2, :2]}")
+secondRow = matrix_1[1, :]
+thridColumn = matrix_1[:, 2]
+subMatrix = matrix_1[:2, :2]
 
 '''
+Ejercicio 3: Álgebra Lineal
+    Dadas las siguientes matrices:
 
+    A = np.array([[1, 2], [3, 4]])
+    B = np.array([[5, 6], [7, 8]])
+
+    - Calcula el producto de las matrices A y B.
+
+    - Calcula la transpuesta de la matriz A.
 '''
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+productMatrix = np.matmul(A, B)
+
+def main():
+    print(f"De la lista{array_1} cada elemento * 3 -5: {operation_1}\n")
+    print(matrix_1)
+    # segunda fila
+    print(f"Segunda fila: {secondRow}")
+    # tercera columna
+    print(f"Tercera columna: {thridColumn}")
+    # dos primeras filas, dos ultimas columnas
+    print(f"Submatriz: \n{subMatrix}\n")
+    print(f"{A}\n * \n{B}\n = \n{productMatrix}")
+
+if __name__ == '__main__':
+    main()
