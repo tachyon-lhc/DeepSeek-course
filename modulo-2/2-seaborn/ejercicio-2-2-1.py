110d65faac22eba7687db85d7c27178487c76489
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2025-02-11 11:39:48 -0300

    Ejercicios seaborn

--- modulo-2/2-seaborn/ejercicio-2-2-1.py
+++ modulo-2/2-seaborn/ejercicio-2-2-1.py
@@ -1,25 +1,26 @@
 # 09/02/2025
-'''
+"""
 Ejercicio 1: Crear un Heatmap de Correlación
     Objetivo:
              Dado un DataFrame (puedes generar uno con datos aleatorios
              o utilizar uno real), calcula la matriz de correlación entre
              sus variables y crea un heatmap.
-'''
+"""
+
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
 data = np.random.rand(10, 5)
-df = pd.DataFrame(data, columns=['A', 'B', 'C', 'D', 'E'])
+df = pd.DataFrame(data, columns=pd.Index(["A", "B", "C", "D", "E"]))
 
 corr_matrix = df.corr()
 
 # Crear un heatmap
 plt.figure(figsize=(8, 6))
-sns.heatmap(corr_matrix, annot=True, cmap='rocket')
-plt.tittle('Matrix de correlacion')
+sns.heatmap(corr_matrix, annot=True, cmap="rocket")
+plt.title("Matrix de correlacion")
 
 print(data)
 print()
